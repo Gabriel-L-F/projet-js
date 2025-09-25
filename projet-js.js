3b1b7dcd8a362b4f3bac84726b4d0ddead6b652d
Gabriel-L-F <gabriel.lefebvre_fristot@edu.devinci.fr>
2025-09-25 09:14:06 +0200

    js

--- projet-js.js
+++ projet-js.js
@@ -1,3 +1,30 @@
 const apiKey = "9f412309bd6e72eefbbbc660";
 const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+async function chargerDevises() {
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                const devises = Object.keys(data.conversion_rates);
 
+                const sourceSelect = document.getElementById("source");
+                const cibleSelect = document.getElementById("cible");
+
+                devises.forEach(code => {
+                    const option1 = document.createElement("option");
+                    option1.value = code;
+                    option1.textContent = code;
+                    sourceSelect.appendChild(option1);
+
+                    const option2 = document.createElement("option");
+                    option2.value = code;
+                    option2.textContent = code;
+                    cibleSelect.appendChild(option2);
+                });
+
+                sourceSelect.value = "USD";
+                cibleSelect.value = "EUR";
+            } catch (error) {
+                console.error("Erreur lors du chargement des devises :", error);
+            }
+        }
+        chargerDevises();
